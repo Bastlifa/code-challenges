ec69ff3e47d3741764f607fb0a67bc79cdbeaf7e
Eli Sacks <bastlifa@gmail.com>
2020-02-25 12:36:21 -0500

    cache for telephone words

--- telephone_words.py
+++ telephone_words.py
@@ -11,23 +11,29 @@ digits_to_letters = {
     8: 'TUV',
     9: 'WXYZ'
 }
-
+cache = {}
 def num_to_word(num_str):
-
+    
     if num_str == '':
         return []
+    if num_str in cache:
+        return cache[num_str]
+
     num_arr = [int(i) for i in num_str]
     new_words =[]
 
     if len(num_arr) == 1:
         for c in digits_to_letters[num_arr[0]]:
             new_words.append(c)
+        cache[num_str] = new_words
         return new_words
         
     for w in num_to_word(num_str[1:]):
         for c in digits_to_letters[num_arr[0]]:
-            new_words.append(c + w)
+            new_words.append(''.join([c,w]))
+            # new_words.append(c + w)
 
+    cache[num_str] = new_words
     return new_words
 
-print(num_to_word('27'))
+print(num_to_word('274'))
